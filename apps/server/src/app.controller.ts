81e1de8878e9ca029e937515e9d4fb5f7c48e24d
snack0verflow <chuck.ng@hivelocity.io>
2024-09-29 02:57:20 +0100

    chore(server): clean up

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,16 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { UserRepository } from './database/user.repo';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly userRepo: UserRepository
-  ) {}
-
   @Get()
   async getHello() {
-    return this.userRepo.list()
+    return {
+      status: 'ok'
+    }
   }
 }
