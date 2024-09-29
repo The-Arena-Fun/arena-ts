dd1b9aa84b02f5410a733b904e711b8680784e56
Aiyualive <liuaiyu1302@gmail.com>
2024-09-30 05:52:32 +0700

    feat: add battlelogo and modify header

--- apps/web/src/components/atoms/Nav/NavHeader.tsx
+++ apps/web/src/components/atoms/Nav/NavHeader.tsx
@@ -9,7 +9,13 @@ export function NavHeader() {
   return (
     <nav className="w-full py-4 px-4 flex flex-row items-center justify-between">
       <Link href="/">
-        <Image src={Logo} width={92} height={30} alt="logo" />
+        <Image
+          className="pl-2"
+          src={Logo}
+          width={138}
+          height={45}
+          alt="logo"
+        />
       </Link>
       <div className="flex flex-row gap-x-4">
         <Balance />
