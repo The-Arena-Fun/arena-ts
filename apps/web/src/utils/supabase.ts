bb3c030311c85b6b4380bdc824e2461716d563b5
snack0verflow <chuck.ng@hivelocity.io>
2024-10-04 23:37:47 +0100

    feat(match-making): shorten address

--- apps/web/src/utils/supabase.ts
+++ apps/web/src/utils/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/../../server/exports'
 
 export const supabase = createClient<Database>(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_ANON_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 )
