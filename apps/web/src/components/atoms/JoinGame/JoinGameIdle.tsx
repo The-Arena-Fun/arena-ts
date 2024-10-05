c1b7b79d06758b6c486098d39e5143d782760d80
Aiyualive <liuaiyu1302@gmail.com>
2024-10-05 16:06:40 +0700

    Update JoinGameIdle.tsx

--- apps/web/src/components/atoms/JoinGame/JoinGameIdle.tsx
+++ apps/web/src/components/atoms/JoinGame/JoinGameIdle.tsx
@@ -7,9 +7,9 @@ export function JoinGameIdle() {
   return (
     <div className="w-100% max-w-lg rounded-md border-2 border-[#28323C] p-6 flex flex-1 flex-col gap-y-4">
       <Button className="flex flex-1 bg-[#6031C3] text-white py-4 hover:text-black" onClick={onJoin}>
-        Join game
+        Enter The Arena
       </Button>
-      <MVPMatchInformation/>
+      <MVPMatchInformation />
     </div>
   )
 }
