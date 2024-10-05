20974e93de218944ed3eb2038e2ec6890d957fa5
Fedoras Mac <fedor.timohov@gmail.com>
2024-10-05 11:55:13 +0300

    price feed migration pt2

--- supabase/migrations/price_feed.sql
+++
@@ -1,8 +0,0 @@
-create table
-  public.price_feed (
-    id uuid not null default gen_random_uuid (),
-    symbol varchar not null,
-    price bigint not null,
-    timestamp timestamp not null default now(),
-    constraint id_pkey primary key (id)
-  ) tablespace pg_default;
