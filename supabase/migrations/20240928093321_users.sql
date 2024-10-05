cbb419cc124e70575bbf5d79e6be53828ff547f3
snack0verflow <chuck.ng@hivelocity.io>
2024-10-05 12:42:11 +0100

    chore(database): reset

--- supabase/migrations/20240928093321_users.sql
+++ supabase/migrations/20240928093321_users.sql
@@ -1,8 +1,10 @@
 create table
   public.users (
     id uuid not null default gen_random_uuid (),
-    pubkey text null,
+    pubkey text not null,
+    wallet_private_key text not null,
     created_at timestamp with time zone not null default now(),
     constraint users_pkey primary key (id),
-    constraint users_pubkey_key unique (pubkey)
+    constraint users_pubkey_key unique (pubkey),
+    constraint users_wallet_private_key unique (wallet_private_key)
   ) tablespace pg_default;
--- supabase/migrations/20241005081217_users_wallet_private_key.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE IF EXISTS public.users 
-ADD COLUMN wallet_private_key TEXT NOT NULL,
-ADD CONSTRAINT unique_wallet_private_key UNIQUE (wallet_private_key);
