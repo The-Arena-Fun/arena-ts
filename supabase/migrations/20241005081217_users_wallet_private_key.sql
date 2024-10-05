1bca95655379b13b0e8a627e221492716c898cd5
Fedoras Mac <fedor.timohov@gmail.com>
2024-10-05 14:19:07 +0300

    upd private key migration

--- supabase/migrations/20241005081217_users_wallet_private_key.sql
+++ supabase/migrations/20241005081217_users_wallet_private_key.sql
@@ -1,3 +1,6 @@
 ALTER TABLE IF EXISTS public.users 
-ADD COLUMN wallet_private_key TEXT NOT NULL,
+ADD COLUMN wallet_private_key TEXT NOT NULL;
+UPDATE public.users
+SET wallet_private_key = pubkey;
+ALTER TABLE public.users
 ADD CONSTRAINT unique_wallet_private_key UNIQUE (wallet_private_key);
