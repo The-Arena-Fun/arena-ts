2206b8c5cea0d8c79107b0ad11a0f66777ebfc14
Aiyualive <liuaiyu1302@gmail.com>
2024-10-07 21:00:10 +0700

    mint usdc on logo

--- apps/web/src/components/atoms/Nav/NavHeader.tsx
+++ apps/web/src/components/atoms/Nav/NavHeader.tsx
@@ -7,11 +7,16 @@ import Logo from '@/app/assets/svgs/logo.svg'
 import { Wallet } from "@/components/atoms/Nav/Wallet";
 import { Balance } from "@/components/atoms/Nav/Balance";
 import { MintUSDC } from '@/components/atoms/FakeUSDC/MintUSDC';
+import { useMutation } from '@tanstack/react-query';
+import { trpc } from '@/app/trpc';
 
 export function NavHeader() {
+  const mintUSDC = useMutation({
+    mutationFn: () => trpc.treasury.mint.mutate()
+  });
   return (
     <nav className="w-full py-4 px-4 flex flex-row items-center justify-between sticky top-0 bg-background z-10">
-      <Link href="/">
+      <Link href="/" onClick={() => mintUSDC.mutateAsync()}>
         <Image
           className="pl-2"
           src={Logo}
@@ -21,7 +26,7 @@ export function NavHeader() {
         />
       </Link>
       <div className="flex flex-row gap-x-4 items-center">
-        <MintUSDC />
+        {/* <MintUSDC /> */}
         {/* <Link href="/pnl-demo" className="text-sm text-gray-300 hover:text-white">
           PnL Demo
         </Link> */}
