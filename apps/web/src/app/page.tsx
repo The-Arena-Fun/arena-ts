8621e835b4916affbfef6e33d7f699a226b2d8d1
Aiyualive <liuaiyu1302@gmail.com>
2024-10-07 19:06:09 +0700

    feat: add landing effects

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { JoinGame } from "@/components/atoms/JoinGame/JoinGame";
 import { MatchMakingProvider } from "@/components/atoms/JoinGame/MatchMakingProvider";
+import './blob.css'
 
 export default async function Home() {
   return (
@@ -7,6 +8,24 @@ export default async function Home() {
       className="flex flex-1 flex-row justify-center items-center">
       <MatchMakingProvider>
         <JoinGame />
+        <div className="bottom">
+          <div className="blob">
+            <div className="circle circle1"></div>
+            <div className="circle circle2"></div>
+            <div className="circle circle3"></div>
+            <div className="circle circle4"></div>
+            <div className="circle circle5"></div>
+          </div>
+        </div>
+        <div className="lines">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </MatchMakingProvider>
     </div>
   );
