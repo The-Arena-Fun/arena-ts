7cdec1309d6af8396fc814e57bf1b58474dd74e7
Aiyualive <liuaiyu1302@gmail.com>
2024-10-08 03:08:16 +0700

    hide stuff

--- apps/web/src/components/atoms/ChatBox/ChatBox.tsx
+++ apps/web/src/components/atoms/ChatBox/ChatBox.tsx
@@ -14,9 +14,9 @@ export function ChatBox() {
   //   }
   // }, [])
   return (
-    <div className="fixed flex flex-col gap-y-4 right-0 bottom-0 h-2/5 w-96 border mr-8 mb-6 rounded-md bg-background p-4">
-      <ChatMessages />
-      <ChatInput/>
+    <div className="fixed flex flex-col gap-y-4 right-0 bottom-0 h-0/5 w-96 border mr-8 mb-6 rounded-md bg-background p-4">
+      {/* <ChatMessages /> */}
+      <ChatInput />
     </div>
   )
 }
