c7b7647d1f15348621daf120096136fcca53a972
Aiyualive <liuaiyu1302@gmail.com>
2024-10-07 22:41:12 +0700

    refactor: change toast duration

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${protoMono.variable} antialiased min-h-screen flex flex-col font-[family-name:var(--font-proto-mono)]`}
       >
         <Providers>
-          <Toaster position={"bottom-left"} />
+          <Toaster position={"bottom-left"} duration={5000} />
           <NavHeader />
           <div className="w-full px-8 flex flex-1">
             {children}
