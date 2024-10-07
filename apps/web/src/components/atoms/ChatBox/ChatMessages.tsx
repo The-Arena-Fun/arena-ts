4717aeb256b196ae44950968e70892cb2f446724
Aiyualive <liuaiyu1302@gmail.com>
2024-10-07 08:34:01 +0700

    fix scrollbar chatbox

--- apps/web/src/components/atoms/ChatBox/ChatMessages.tsx
+++ apps/web/src/components/atoms/ChatBox/ChatMessages.tsx
@@ -6,7 +6,7 @@ import { ChatMessageItem } from './ChatMessageItem'
 export function ChatMessages() {
   const messagesQuery = useChatMessages()
   return (
-    <div className="w-full flex flex-1 flex-col gap-y-4 overflow-y-scroll">
+    <div className="w-full flex flex-1 flex-col gap-y-4 overflow-y-auto">
       {messagesQuery.data?.map((item, index) => (
         <ChatMessageItem
           key={index.toString()}
