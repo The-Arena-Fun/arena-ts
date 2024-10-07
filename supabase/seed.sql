ad4db08f4f50a7ce410c4520cc1f4f2e6d3d50e9
Aiyualive <liuaiyu1302@gmail.com>
2024-10-07 08:32:42 +0700

    feat: seed fake usdc

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -2,4 +2,5 @@ insert into support_tokens
   (name, token_pubkey)
 values
   ('USDC', 'FSxJ85FXVsXSr51SeWf9ciJWTcRnqKFSmBgRDeL3KyWw'),
-  ('USDC (devnet)', '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU')
+  ('USDC (devnet)', '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'),
+  ('USDC (fake)', 'DhY8Ev1dWJChmx1fj6RwMnWrisx8LziKuJq7rXmYYo1s')
